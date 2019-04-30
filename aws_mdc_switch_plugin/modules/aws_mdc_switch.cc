48138ed0ac627871ccf7075c4085e929525fd72d
carlosl <carlosl@sfu.ca>
2019-04-29 17:25:34 -0700

    asdf

--- aws_mdc_switch_plugin/modules/aws_mdc_switch.cc
+++ aws_mdc_switch_plugin/modules/aws_mdc_switch.cc
@@ -155,13 +155,13 @@ void AwsMdcSwitch::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
         // Data pkts
         uint8_t mode = p->raw_value() & 0x00ff0000;
         if (mode == 0x00) {
-            // If mode is 0x00, the data pkt needs to be forwarded to the active agent
-            std::cout << "Forwarded"<< std::endl;
-            EmitPacket(ctx, pkt, active_agent_id_);
-        } else {
-            std::cout << "DropPacket"<< std::endl;
-            DropPacket(ctx, pkt);
-            continue;
+        //     // If mode is 0x00, the data pkt needs to be forwarded to the active agent
+        //     std::cout << "Forwarded"<< std::endl;
+        //     EmitPacket(ctx, pkt, active_agent_id_);
+        // } else {
+        //     std::cout << "DropPacket"<< std::endl;
+        //     DropPacket(ctx, pkt);
+        //     continue;
 
             // Let's check the label
             uint8_t label = p->raw_value() & 0xff000000;
