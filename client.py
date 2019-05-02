90281fa34728fd3540da71bb078bcab776cc54fb
Carlos Lee <charlee593@gmail.com>
2019-05-02 01:31:58 -0700

    s

--- client.py
+++ client.py
@@ -13,16 +13,15 @@ print('Data pkt size = %d' % data_pkt_size)
 
 # Data pkt
 class MDCData(scapy.Packet):
-    fields_desc=[scapy.ShortField('addr', 0),
-                 scapy.XByteField('mode', 0),
-                 scapy.XByteField('label' , 0)]
+    name = 'MulticastDataCenterData '
+    fields_desc=[scapy.ShortField('addr', 0)]
 
 
 # Data pkt template
 data_eth = scapy.Ether(src='aa:bb:cc:dd:ee:01', dst='11:22:33:44:55:01', type=0x0800)
 data_ip = scapy.IP(src='10.1.0.1', dst='10.0.0.1')
 data_udp = scapy.UDP(sport=10001, dport=10002)
-unlabeled_data_mdc = MDCData(addr=0x1a1b, mode=0x01, label=0x0)
+unlabeled_data_mdc = MDCData(addr=0x1a1b)
 
 print("HERER.", len(bytes(unlabeled_data_mdc)))
 
