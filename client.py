6d4da273377a15029a80cf2763a952b5516fed0c
carlosl <carlosl@sfu.ca>
2019-05-03 16:07:01 -0700

    s

--- client.py
+++ client.py
@@ -60,7 +60,7 @@ if os.path.exists("/tmp/mdc_dp_p.sock"):
                 data_ip_header = data[len(bytes(data_eth)):len(bytes(data_eth))+len(bytes(data_ip))]
                 data_udp_header = data[len(bytes(data_eth))+len(bytes(data_ip)):len(bytes(data_eth))+len(bytes(data_ip))+len(bytes(data_udp))]
                 unlabeled_data_mdc_header = data[len(bytes(data_eth))+len(bytes(data_ip))+len(bytes(data_udp)):]
-                # print("unlabeled_data_mdc_header: " struct.unpack('>H', unlabeled_data_mdc_header[8:]) " " unlabeled_data_mdc_header[8:])
+                print("unlabeled_data_mdc_header: ", struct.unpack('>H', unlabeled_data_mdc_header[8:]), " ", unlabeled_data_mdc_header[8:])
                 recv_cnt += 1
                 print("S ", sending_pk_sn, "R ", recv_cnt)
 
