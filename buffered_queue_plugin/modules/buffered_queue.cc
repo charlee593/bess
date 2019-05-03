8d2712a8a7bb73c13404ff8067053701f8702d7b
carlosl <carlosl@sfu.ca>
2019-05-03 16:41:06 -0700

    d

--- buffered_queue_plugin/modules/buffered_queue.cc
+++ buffered_queue_plugin/modules/buffered_queue.cc
@@ -167,6 +167,7 @@ void BufferedQueue::ProcessBatch(Context *, bess::PacketBatch *batch) {
     std::cout << std::hex << (p->raw_value() >> 4)  << std::endl;
     std::cout << std::hex << p->raw_value()  << std::endl;
     std::cout <<  pkt << std::endl;
+    std::cout <<  p << std::endl;
 
 
 
