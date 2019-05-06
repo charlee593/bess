11bcfd9259023207df96690ce33a6b0278d73f15
Carlos Lee <charlee593@gmail.com>
2019-05-06 03:12:28 -0700

    no message

--- buffered_queue_plugin/modules/buffered_queue.cc
+++ buffered_queue_plugin/modules/buffered_queue.cc
@@ -180,7 +180,7 @@ void BufferedQueue::ProcessBatch(Context *, bess::PacketBatch *batch) {
     // Data pkts
     uint8_t sn = (p->raw_value() & 0xff);
     uint8_t mul_type = (p->raw_value() & 0xff00)  >> 8;
-    uint16_t data_size = (p->raw_value() & 0xff0000)  >> 16;
+    uint16_t data_size = (p->raw_value() & 0xffff0000)  >> 16;
     
     uint16_t address = (p->raw_value() & 0x0000ffff);
     uint8_t appID = (p->raw_value() & 0xff00000000) >> 32;
@@ -212,7 +212,7 @@ void BufferedQueue::ProcessBatch(Context *, bess::PacketBatch *batch) {
       if(curr_ == (prior_+1)%data_size_ ){
         prior_ = curr_;
       }
-      else if(curr_ >= (prior_+1)%data_size_  || curr_ <= initial_){
+      else if(curr_ > (prior_+1)%data_size_  || curr_ <= initial_){
         std::cout << "Case 2" << std::endl;
         return;
 
