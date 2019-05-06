c9d08656acc5aa7a410c433b7749a9c609345798
Carlos Lee <charlee593@gmail.com>
2019-05-06 11:30:39 -0700

    no message

--- buffered_queue_plugin/modules/buffered_queue.cc
+++ buffered_queue_plugin/modules/buffered_queue.cc
@@ -205,7 +205,7 @@ void BufferedQueue::ProcessBatch(Context *, bess::PacketBatch *batch) {
 
 
     Ethernet *new_eth = new_pkt->head_data<Ethernet *>();
-    Ipv4 *new_ip = reinterpret_cast<Ipv4 *>(new_eth + 1);
+    // Ipv4 *new_ip = reinterpret_cast<Ipv4 *>(new_eth + 1);
     be64_t *p = new_pkt->head_data<be64_t *>(sizeof(Ethernet) + ip_bytes + sizeof(Udp));
     p->raw_value() = 3;
     std::cout << "BufferedQueue new packet"  + std::to_string(new_pkt->head_data<be64_t *>(sizeof(Ethernet) + ip_bytes + sizeof(Udp))) << std::endl;
