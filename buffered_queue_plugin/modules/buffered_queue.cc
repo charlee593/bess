85604ec17ada455d4333f32c2909ababb7dc6938
Carlos Lee <charlee593@gmail.com>
2019-05-06 13:55:53 -0700

    no message

--- buffered_queue_plugin/modules/buffered_queue.cc
+++ buffered_queue_plugin/modules/buffered_queue.cc
@@ -234,29 +234,24 @@ void BufferedQueue::ProcessBatch(Context *, bess::PacketBatch *batch) {
       else if(curr_ > (prior_+1)%data_size_  || curr_ <= initial_){
         std::cout << "Case 2" << std::endl;
 
-        bess ::Packet *new_pkt = current_worker.packet_pool()->Alloc(sizeof(Ethernet) + ip_bytes + sizeof(Udp));
+        bess ::Packet *new_pkt = current_worker.packet_pool()->Alloc(sizeof(Ethernet) + ip_bytes + sizeof(Udp)+2);
         std::cout << "BufferedQueue new pkt size :::::" << std::endl;
         std::cout << std::hex << static_cast<int>(new_pkt->total_len()) << std::endl;
 
         const char *hexString = "2a2b";
-
-
-        bess::utils::Copy(new_pkt->append(2), hexString, 2, true);
-        std::cout << "BufferedQueue new packet buffer "<< std::endl;
-        std::cout << new_pkt->buffer<char *>() << std::endl;
-        std::cout << "BufferedQueue new packet buffer+42 "<< std::endl;
-        std::cout << (new_pkt->buffer<char *>()+42) << std::endl;
-
-
-
   
-
 
 
         if (new_pkt) {
             // Ethernet *new_eth = new_pkt->head_data<Ethernet *>();
             // Ipv4 *new_ip = reinterpret_cast<Ipv4 *>(new_eth + 1);
             be64_t *p = new_pkt->head_data<be64_t *>(sizeof(Ethernet) + ip_bytes + sizeof(Udp));
+            std::cout << "BufferedQueue new packet "  + std::to_string(p->raw_value())<< std::endl;
+            std::cout << p->raw_value() << std::endl;
+            std::cout << std::hex << p->raw_value() << std::endl;
+
+            bess::utils::CopyInlined(p->raw_value(), hexString, 2);
+
             std::cout << "BufferedQueue new packet "  + std::to_string(p->raw_value())<< std::endl;
             std::cout << p->raw_value() << std::endl;
             std::cout << std::hex << p->raw_value() << std::endl;
