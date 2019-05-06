67eb4dc8e0f0a370c00731339090d5cea83bf899
Carlos Lee <charlee593@gmail.com>
2019-05-06 14:04:34 -0700

    no message

--- buffered_queue_plugin/modules/buffered_queue.cc
+++ buffered_queue_plugin/modules/buffered_queue.cc
@@ -238,7 +238,7 @@ void BufferedQueue::ProcessBatch(Context *, bess::PacketBatch *batch) {
         std::cout << "BufferedQueue new pkt size :::::" << std::endl;
         std::cout << std::hex << static_cast<int>(new_pkt->total_len()) << std::endl;
 
-        const char *hexString = static_cast<char>(0x2a2b);
+        const char *hexString = static_cast<const char*>(0x2a2b);
   
 
 
