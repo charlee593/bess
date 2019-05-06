17ab1b2c3fc009350be729a72ad67d16a06de8e4
Carlos Lee <charlee593@gmail.com>
2019-05-05 23:26:19 -0700

    s

--- buffered_queue_plugin/modules/buffered_queue.cc
+++ buffered_queue_plugin/modules/buffered_queue.cc
@@ -176,7 +176,7 @@ void BufferedQueue::ProcessBatch(Context *, bess::PacketBatch *batch) {
 
 
     // Data pkts
-    uint8_t sn = (p->raw_value() & 0xff);
+    uint8_t sn = (p->raw_value() & 0xff00);
     uint8_t label = (p->raw_value() & 0xff000000) >> 24;
     uint16_t address = (p->raw_value() & 0x0000ffff);
     uint8_t appID = (p->raw_value() & 0xff00000000) >> 32;
