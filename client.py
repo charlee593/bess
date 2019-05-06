c840c16bcd194e444e9227224ef9a6e533c90d2a
Carlos Lee <charlee593@gmail.com>
2019-05-06 02:07:40 -0700

    r

--- client.py
+++ client.py
@@ -11,7 +11,8 @@ class MDCData(scapy.Packet):
                  scapy.XByteField('label' , 0),
                  scapy.ShortField('appID' , 2),
                  scapy.ShortField('dataID' , 0),
-                 scapy.XByteField('sn' , 0)]
+                 scapy.XByteField('sn' , 0),
+                 scapy.ByteEnumField("code", 5, {1:"REQUEST",2:"PTCH_REQ",3:"DATA_FNSD",4:"PTCH_DATA", 5:"DATA"})]
 
 
 
