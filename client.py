2b7aa9b26e91650f87a64d7c3aac5c5ae4b79da7
Carlos Lee <charlee593@gmail.com>
2019-05-07 12:26:21 -0700

    no message

--- client.py
+++ client.py
@@ -41,7 +41,7 @@ if os.path.exists("/tmp/mdc_dp_p.sock"):
     sending_pk_sn = 0
     recv_cnt = 0
 
-    while sending_pk_sn < 200:
+    while sending_pk_sn < 10:
         try:
             unlabeled_data_pkt[MDCData].sn = sending_pk_sn
             # print("Sendiing unlabeled_data_mdc_header: ", bytes(unlabeled_data_pkt[MDCData]).encode('hex'))
