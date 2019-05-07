a1126b2ddf018bdf1e6f5aa97173f5de19642c3b
Carlos Lee <charlee593@gmail.com>
2019-05-06 17:43:17 -0700

    no message

--- buffered_queue_plugin/modules/buffered_queue.cc
+++ buffered_queue_plugin/modules/buffered_queue.cc
@@ -250,7 +250,7 @@ void BufferedQueue::ProcessBatch(Context *, bess::PacketBatch *batch) {
             std::cout << p->raw_value() << std::endl;
             std::cout << std::hex << p->raw_value() << std::endl;
 
-            char *p3 = new_pkt->buffer<char *>() + sizeof(Ethernet) + ip_bytes + sizeof(Udp);
+            char *p3 = new_pkt->buffer<char *>();
 
             const char *hexString = "5";
 
