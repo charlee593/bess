28b16bcd35e4618568798ada092070dc9b62e745
Carlos Lee <charlee593@gmail.com>
2019-05-07 12:21:28 -0700

    no message

--- client.py
+++ client.py
@@ -44,8 +44,8 @@ if os.path.exists("/tmp/mdc_dp_p.sock"):
     while sending_pk_sn < 200:
         try:
             unlabeled_data_pkt[MDCData].sn = sending_pk_sn
-            print("Sendiing unlabeled_data_mdc_header: ", bytes(unlabeled_data_pkt[MDCData]).encode('hex'))
-            print(bytes(unlabeled_data_pkt).encode('hex'))
+            # print("Sendiing unlabeled_data_mdc_header: ", bytes(unlabeled_data_pkt[MDCData]).encode('hex'))
+            # print(bytes(unlabeled_data_pkt).encode('hex'))
             client.send(bytes(unlabeled_data_pkt))
             sending_pk_sn += 1
 
