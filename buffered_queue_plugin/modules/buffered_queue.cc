c1dbf768401353d2543c25c4adb75fd40bf44e2b
Carlos Lee <charlee593@gmail.com>
2019-05-07 10:45:03 -0700

    no message

--- buffered_queue_plugin/modules/buffered_queue.cc
+++ buffered_queue_plugin/modules/buffered_queue.cc
@@ -176,8 +176,7 @@ void BufferedQueue::SendReq(uint8_t code, uint8_t lrange, uint8_t rrange,
   int ip_bytes = ip->header_length << 2;
 
   if (new_pkt) {
-      be64_t *new_p = new_pkt->head_data<be64_t *>
-      (sizeof(Ethernet) + ip_bytes + sizeof(Udp)); // First 8 bytes
+      be64_t *new_p = new_pkt->head_data<be64_t *>(sizeof(Ethernet) + ip_bytes + sizeof(Udp)); // First 8 bytes
 
       uint64_t mDC = 0xffff & addr;
       mDC = mDC | (0xff0000 & mode);
@@ -186,14 +185,16 @@ void BufferedQueue::SendReq(uint8_t code, uint8_t lrange, uint8_t rrange,
       mDC = mDC | (0xff0000000000 & app_id);
       mDC = mDC | (0xff000000000000 & data_id);
       mDC = mDC | (0xff00000000000000 & lrange);
-      bess::utils::Copy(new_p, reinterpret_cast<uint64_t *>(&mDC), 64);
+      bess::utils::Copy(new_p, reinterpret_cast<uint64_t *>(&mDC), 16);
 
       new_p = new_pkt->head_data<be64_t *>(sizeof(Ethernet) + ip_bytes + sizeof(Udp) + 8);
-      bess::utils::Copy(new_p, reinterpret_cast<uint64_t *>(&rrange), 8);
+      bess::utils::Copy(new_p, reinterpret_cast<uint64_t *>(&rrange), 2);
 
 
       be64_t *p4 = new_pkt->head_data<be64_t *>(sizeof(Ethernet) + ip_bytes + sizeof(Udp));
       std::cout << "BufferedQueue new packet "  << p4->raw_value() << std::endl;
+      be64_t *p3 = new_pkt->head_data<be64_t *>(sizeof(Ethernet) + ip_bytes + sizeof(Udp) + 2);
+      std::cout << "BufferedQueue new packet "  << p3->raw_value() << std::endl;
 
       // EmitPacket(ctx, new_pkt, i);
   }
