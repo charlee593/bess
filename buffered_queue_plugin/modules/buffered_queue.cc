15806774bbd4dc2fa02891ff8f49a5f8bd6f8f57
carlosl <carlosl@sfu.ca>
2019-05-23 16:33:58 -0700

    s

--- buffered_queue_plugin/modules/buffered_queue.cc
+++ buffered_queue_plugin/modules/buffered_queue.cc
@@ -260,7 +260,8 @@ void BufferedQueue::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
     bess::utils::CuckooMap<uint8_t, RecverState> cuckoo;
     cuckoo.Insert(app_id, *recv_p);
     auto result = cuckoo.Find(app_id);
-    std::cout << "CuckooMap: " << result->second << std::endl;
+    RecverState * recv_r = result->second ;
+    std::cout << "CuckooMap: " << recv_r->bcd_filename << std::endl;
 
     // SendReq(0x02, prior_, 0xcc, app_id, data_id, mode, label, addr, ctx);
     // RunNextModule(ctx, batch);
