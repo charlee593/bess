05a358cfcc2b851c9eb88779a66e8b49e648f674
carlosl <carlosl@sfu.ca>
2019-05-28 12:24:02 -0700

    d

--- file_writer_plugin/modules/file_writer.cc
+++ file_writer_plugin/modules/file_writer.cc
@@ -98,7 +98,9 @@ void FileWriter::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
 
     std::cout << "FileWriter ProcessBatch head_data: "  << std::endl;
     std::cout << std::hex << pkt->data() << std::endl;
-    std::cout << std::bitset<8>(pkt->head_data<char *>(offset)) << std::endl;
+    // std::cout << std::bitset<8>(pkt->head_data<char *>(offset)) << std::endl;
+    std::bitset<8> x(pkt->head_data<char *>(offset);
+    std::cout << x << '\n';
 
     // fwrite to fd_d
     uint8_t data_written = fwrite( pkt->head_data<void *>(offset) , sizeof(char), 1, fd_d);
