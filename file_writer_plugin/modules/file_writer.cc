7db926d59ec8a43242daf24af63e72034d8e38e1
carlosl <carlosl@sfu.ca>
2019-05-28 10:30:45 -0700

    s

--- file_writer_plugin/modules/file_writer.cc
+++ file_writer_plugin/modules/file_writer.cc
@@ -99,7 +99,7 @@ void FileWriter::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
     std::cout << "FileWriter ProcessBatch head_data: "  << std::endl;
     std::cout << std::hex << pkt->data() << std::endl;
     std::cout << std::hex << pkt->head_data<char *>(offset) << std::endl;
-    std::cout << std::hex << mdc_p1->raw_value() << std::endl;
+    std::cout << std::hex << &mdc_p1 << std::endl;
 
 
 
