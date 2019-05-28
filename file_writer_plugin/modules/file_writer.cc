7218022c4f7bb5b164e42789aa793ba6f0d809de
carlosl <carlosl@sfu.ca>
2019-05-28 13:02:49 -0700

    d

--- file_writer_plugin/modules/file_writer.cc
+++ file_writer_plugin/modules/file_writer.cc
@@ -68,43 +68,14 @@ void FileWriter::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
 
     // Access UDP payload (i.e., mDC data)
     uint8_t offset = sizeof(Ethernet) + ip_bytes + sizeof(Udp);
-    be64_t *mdc_p1 = pkt->head_data<be64_t *>(offset); // first 8 bytes
-    be64_t *mdc_p2 = pkt->head_data<be64_t *>(offset + 8); // second 8 bytes
-
-    uint16_t addr = (mdc_p1->raw_value() & 0xffff);
-    uint8_t mode = (mdc_p1->raw_value() & 0xff0000) >> 16;
-    uint8_t label = (mdc_p1->raw_value() & 0xff000000) >> 24;
-    uint8_t code = (mdc_p1->raw_value() & 0xff00000000) >> 32;
-    uint8_t app_id = (mdc_p1->raw_value() & 0xff0000000000) >> 40;
-    uint8_t data_id = (mdc_p1->raw_value() & 0xff000000000000) >> 48;
-    uint8_t sn = (mdc_p1->raw_value() & 0xff00000000000000) >> 56;
-    uint8_t data_size = (mdc_p2->raw_value() & 0xff);
-
-    std::cout << "FileWriter ProcessBatch batch size: " + std::to_string(cnt) + " pkt: " + std::to_string(i) << std::endl;
-    std::cout << std::hex << addr << std::endl;
-    std::cout << std::hex << static_cast<int>(mode) << std::endl;
-    std::cout << std::hex << static_cast<int>(label) << std::endl;
-    std::cout << std::hex << static_cast<int>(code) << std::endl;
-    std::cout << std::hex << static_cast<int>(app_id) << std::endl;
-    std::cout << std::hex << static_cast<int>(data_id) << std::endl;
-    std::cout << std::hex << std::to_string(sn) << std::endl;
-    std::cout << std::hex << std::to_string(data_size) << std::endl;
 
     FILE * fd_d = get_attr<FILE *>(this, ATTR_R_FILE_D, pkt);
-    uint8_t data_id_ = get_attr<uint8_t>(this, ATTR_R_HEADER_SIZE, pkt);
+    uint8_t header_size_ = get_attr<uint8_t>(this, ATTR_R_HEADER_SIZE, pkt);
     uint8_t data_size_ = get_attr<uint8_t>(this, ATTR_R_DATA_SIZE, pkt);
 
-    std::cout << "FileWriter ProcessBatch get_attr: " + std::to_string(data_id_) + ", " + std::to_string(data_size_) << fd_d << std::endl;
-
-    std::cout << "FileWriter ProcessBatch head_data: "  << std::endl;
-    std::cout << std::hex << pkt->data() << std::endl;
-    std::cout <<  std::hex << (*(reinterpret_cast<uint8_t *>(pkt->head_data<void *>(offset))) | 0x00) << std::endl;
-
     // fwrite to fd_d
-    uint8_t data_written = fwrite( pkt->head_data<void *>(offset) , sizeof(char), 1, fd_d);
+    uint8_t data_written = fwrite( pkt->head_data<void *>(offset + header_size_) , sizeof(char), data_size_, fd_d);
     fclose (fd_d);
-
-    std::cout << "FileWriter ProcessBatch written: " + std::to_string(data_written)  << std::endl;
 
     // Reply with data id, amount of data written,
     // Rewrite mode field to 0x14
