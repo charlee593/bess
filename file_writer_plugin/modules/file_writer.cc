45794118f88d05531c3942d1cefb066fb1475178
carlosl <carlosl@sfu.ca>
2019-05-30 11:44:57 -0700

    s

--- file_writer_plugin/modules/file_writer.cc
+++ file_writer_plugin/modules/file_writer.cc
@@ -74,6 +74,28 @@ void FileWriter::ProcessBatch(Context *ctx, bess::PacketBatch *batch)
     uint8_t header_size_ = get_attr<uint8_t>(this, ATTR_R_HEADER_SIZE, pkt);
     uint8_t data_size_ = get_attr<uint8_t>(this, ATTR_R_DATA_SIZE, pkt);
 
+    uint8_t offset = sizeof(Ethernet) + ip_bytes + sizeof(Udp);
+    be64_t *mdc_p1 = pkt->head_data<be64_t *>(offset);     // first 8 bytes
+    be64_t *mdc_p2 = pkt->head_data<be64_t *>(offset + 8); // second 8 bytes
+
+    uint16_t addr = (mdc_p1->raw_value() & 0xffff);
+    uint8_t mode = (mdc_p1->raw_value() & 0xff0000) >> 16;
+    uint8_t label = (mdc_p1->raw_value() & 0xff000000) >> 24;
+    uint8_t code = (mdc_p1->raw_value() & 0xff00000000) >> 32;
+    uint8_t app_id = (mdc_p1->raw_value() & 0xff0000000000) >> 40;
+    uint8_t data_id = (mdc_p1->raw_value() & 0xff000000000000) >> 48;
+    uint8_t sn = (mdc_p1->raw_value() & 0xff00000000000000) >> 56;
+    uint8_t data_size = (mdc_p2->raw_value() & 0xff);
+    std::cout << "FileWriter ProcessBatch batch size: " + std::to_string(cnt) + " pkt: " + std::to_string(i) << std::endl;
+    std::cout << std::hex << addr << std::endl;
+    std::cout << std::hex << static_cast<int>(mode) << std::endl;
+    std::cout << std::hex << static_cast<int>(label) << std::endl;
+    std::cout << std::hex << static_cast<int>(code) << std::endl;
+    std::cout << std::hex << static_cast<int>(app_id) << std::endl;
+    std::cout << std::hex << static_cast<int>(data_id) << std::endl;
+    std::cout << std::hex << std::to_string(sn) << std::endl;
+    std::cout << std::hex << std::to_string(data_size) << std::endl;
+
     // fwrite to fd_d
     uint8_t data_written = fwrite(pkt->head_data<void *>(offset + header_size_), sizeof(char), data_size_, fd_d);
     fclose(fd_d);
@@ -85,6 +107,16 @@ void FileWriter::ProcessBatch(Context *ctx, bess::PacketBatch *batch)
     uint8_t *data_size_p = reinterpret_cast<uint8_t *>(head + 8);
     *code_p = (*code_p & 0x00) | 0x06;
     *data_size_p = (*data_size_p & 0x00) | data_written;
+
+    be64_t *mdc_p1_ = pkt->head_data<be64_t *>(offset);     // first 8 bytes
+    be64_t *mdc_p2_ = pkt->head_data<be64_t *>(offset + 8); // second 8 bytes
+
+    uint8_t code_ = (mdc_p1_->raw_value() & 0xff00000000) >> 32;
+    uint8_t data_size = (mdc_p2_->raw_value() & 0xff);
+
+    std::cout << "FileWriter send reply: " << std::endl;
+    std::cout << std::hex << static_cast<int>(code_) << std::endl;
+    std::cout << std::hex << std::to_string(data_size) << std::endl;
 
     EmitPacket(ctx, pkt, 0);
   }
