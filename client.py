6e3ef5a9b497b02b048bf541ba51e6b7bd25c61c
carlosl <carlosl@sfu.ca>
2019-06-12 16:48:07 -0700

    Example Client, send hello world to controller

--- client.py
+++ client.py
@@ -43,16 +43,13 @@ if os.path.exists("/tmp/mdc_dp_p.sock"):
     print("Ready.")
 
     sending_pk_sn = 0
-    recv_cnt = 0
-
-    while sending_pk_sn < 1:
+    while sending_pk_sn < 3:
         try:
             unlabeled_data_pkt[MDCData].sn = sending_pk_sn
             print(
-                "Sendiing unlabeled_data_mdc_header: ",
+                "Sending \"Hello World\" with packet header: ",
                 bytes(unlabeled_data_pkt[MDCData]).encode("hex"),
             )
-            print(bytes(unlabeled_data_pkt).encode("hex"))
             client.send(bytes(unlabeled_data_pkt /"Hello World") )
             sending_pk_sn += 1
 
@@ -60,74 +57,6 @@ if os.path.exists("/tmp/mdc_dp_p.sock"):
             print("Shutting down.")
             client.close()
             break
-
-    while True:
-        try:
-            data = client.recv(len(bytes(unlabeled_data_pkt)))
-
-            if len(data) > 0:
-                data_eth_header = data[: len(bytes(data_eth))]
-                data_ip_header = data[
-                    len(bytes(data_eth)) : len(bytes(data_eth)) + len(bytes(data_ip))
-                ]
-                data_udp_header = data[
-                    len(bytes(data_eth))
-                    + len(bytes(data_ip)) : len(bytes(data_eth))
-                    + len(bytes(data_ip))
-                    + len(bytes(data_udp))
-                ]
-                unlabeled_data_mdc_header = data[
-                    len(bytes(data_eth)) + len(bytes(data_ip)) + len(bytes(data_udp)) :
-                ]
-                print("unlabeled_data_mdc_header: ", unlabeled_data_mdc_header)
-
-                print(
-                    "unlabeled_data_mdc_header: ",
-                    struct.unpack(">B", unlabeled_data_mdc_header[8:9]),
-                    " ",
-                    unlabeled_data_mdc_header[8:9],
-                )
-                print(
-                    "unlabeled_data_mdc_header addr: ",
-                    struct.unpack(">H", unlabeled_data_mdc_header[:2]),
-                    " ",
-                    unlabeled_data_mdc_header[:2],
-                )
-                print(
-                    "unlabeled_data_mdc_header mode: ",
-                    struct.unpack(">B", unlabeled_data_mdc_header[2:3]),
-                    " ",
-                    unlabeled_data_mdc_header[2:3],
-                )
-
-                sending_pk_sn = 0
-
-                while sending_pk_sn < 100:
-                    try:
-                        unlabeled_data_pkt[MDCData].sn = sending_pk_sn
-                        unlabeled_data_pkt[MDCData].code = 4
-                        # print("Sendiing unlabeled_data_mdc_header: ", bytes(unlabeled_data_pkt[MDCData]).encode('hex'))
-                        # print(bytes(unlabeled_data_pkt).encode('hex'))
-                        client.send(bytes(unlabeled_data_pkt))
-                        sending_pk_sn += 1
-
-                    except KeyboardInterrupt as k:
-                        print("Shutting down.")
-                        client.close()
-                        break
-
-                recv_cnt += 1
-                print("S ", sending_pk_sn, "R ", recv_cnt)
-
-        except KeyboardInterrupt as k:
-            print("Shutting down.")
-            client.close()
-            break
-
-        except IOError as e:  # and here it is handeled
-            pass
-
-
 else:
     print("Couldn't Connect!")
     print("Done")
